c47014fa80f4fb309f5c047e6c574d58eaaf8fde
okesmez <omer.faruk.kesmez@gmail.com>
2021-12-30 08:14:35 +0300

    Örnek kullanım düzeltildi.

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require_once 'autoload.php';
  * Facebook JSON : http://case.okesmez.com/api/v1/products/facebook/json
  * Twitter XML   : http://case.okesmez.com/api/v1/products/twitter/xml
  * Twitter JSON  : http://case.okesmez.com/api/v1/products/twitter/json
- * http://feed.okesmez.com/?platform=facebook&format=json
+ * http://case.okesmez.com/?platform=facebook&format=json
  */
 Request::capture();
 
